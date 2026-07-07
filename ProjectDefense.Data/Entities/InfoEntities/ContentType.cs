cc21f00725e58a981fa01d06cbb2a8ab5e9f6f7a
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-07-07 20:37:45 +0900

    InfoContentType

---
+++ ProjectDefense.Data/Entities/InfoEntities/ContentType.cs
@@ -0,0 +1,15 @@
+﻿using ProjectDefense.Data.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDefense.Data.Entities.InfoEntities
+{
+    [Table("info_content_type", Schema = "info")]
+    public class ContentType : BaseInfoEntity
+    {
+        [Required]
+        [Column("type_name")]
+        [MaxLength(100)]
+        public string TypeName { get; set; } = string.Empty;
+    }
+}
