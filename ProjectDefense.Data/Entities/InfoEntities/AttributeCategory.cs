6859ffe755f181453793fd36a73b846f0a77869e
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-07-07 20:33:01 +0900

    InfoAttributeCategory and InfoAttributeType

---
+++ ProjectDefense.Data/Entities/InfoEntities/AttributeCategory.cs
@@ -0,0 +1,11 @@
+﻿using ProjectDefense.Data.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDefense.Data.Entities.InfoEntities
+{
+    [Table("info_attribute_category", Schema = "info")]
+    public class AttributeCategory : BaseInfoEntity
+    {
+
+    }
+}
---
+++ ProjectDefense.Data/Entities/InfoEntities/AttributeType.cs
@@ -0,0 +1,10 @@
+﻿using ProjectDefense.Data.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDefense.Data.Entities.InfoEntities
+{
+    [Table("info_attribute_type", Schema = "info")]
+    public class AttributeType : BaseInfoEntity
+    {
+    }
+}
