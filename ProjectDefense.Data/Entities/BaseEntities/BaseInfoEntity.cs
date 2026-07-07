85a85e49045e26b3b334b2838d85cecf4d4951b3
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-07-07 20:29:32 +0900

    BaseInfoEntity

---
+++ ProjectDefense.Data/Entities/BaseEntities/BaseInfoEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDefense.Data.Entities.BaseEntities
+{
+    public class BaseInfoEntity : BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public short Id { get; set; }
+
+        [Required]
+        [Column("code")]
+        public short Code { get; set; }
+
+        [Required]
+        [Column("name")]
+        [MaxLength(50)]
+        public string Name { get; set; }  = string.Empty;
+
+    }
+}
--- ProjectDefense.Data/obj/Debug/net8.0/ProjectDefense.Data.AssemblyInfo.cs
+++ ProjectDefense.Data/obj/Debug/net8.0/ProjectDefense.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4914e830fcb72b9ae7091623182f78fb462e827f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55903b3c0a88e72f032764c3585f6de1547a4c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
