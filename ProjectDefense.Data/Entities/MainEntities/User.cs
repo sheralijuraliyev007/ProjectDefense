cf86d4778b8aaf5e3d151be3ce52155dd93692d1
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-07-08 15:10:31 +0900

    ProjectDefense.Client is added

---
+++ ProjectDefense.Data/Entities/MainEntities/User.cs
@@ -0,0 +1,24 @@
+﻿using ProjectDefense.Data.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDefense.Data.Entities.MainEntities
+{
+    public class User : BaseEntity
+    {
+        [Key]
+        [Column("users")]
+        public Guid Id { get; set; }
+
+        [Column("email")]
+        [MaxLength(255)]
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+    //    id uuid primary key,
+    //email                varchar(255) not null unique,
+    //is_verified boolean not null default false,
+    //status_code smallint not null references info.info_user_status(code) default 1,
+    //version int not null default 1,
+    }
+}
--- ProjectDefense.Data/obj/Debug/net8.0/ProjectDefense.Data.AssemblyInfo.cs
+++ ProjectDefense.Data/obj/Debug/net8.0/ProjectDefense.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e1a193345e9e02907ac42d8b97db987fe7bf9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1ad49eee24fcb6762f3a04f68a3bf777258885")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectDefense.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
